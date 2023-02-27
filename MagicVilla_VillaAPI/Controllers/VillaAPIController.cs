039b9055649b5b51e28bc143122a7ad2c61b5264
Jaseela93 <jaseelajamal93@gmail.com>
2023-02-27 08:02:26 +0400

    feb27

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,35 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+   // [Route("api/[Controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas() 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTo>> GetVillas() 
         {
-            return new List<Villa>
-            {
-                new Villa {Id=1,Name="Pool View"},
-                new Villa {Id=2,Name="Beach View"}
-            };
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+       // [ProducesResponseType(200)]
+        //[ProducesResponseType(404)]
+       // [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTo> GetVilla(int id)
+        {
+            if(id==0) { return BadRequest(); }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null) { return NotFound(); }
+            return Ok(villa);
         }
     }
 }
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTo> villaList= new List<VillaDTo>
+            {
+                new VillaDTo {Id=1,Name="Pool View"},
+                new VillaDTo {Id=2,Name="Beach View"}
+            };
+    }
+}
